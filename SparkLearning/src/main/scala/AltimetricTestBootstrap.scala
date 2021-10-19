efacfac3a15c5ec916c4b4e7545a7ed466ec02ce
yha7 <hayuvaraj90@gmail.com>
2021-10-19 12:22:55 +0530

    AltimetricTest-Usecase

---
+++ SparkLearning/src/main/scala/AltimetricTestBootstrap.scala
@@ -0,0 +1,66 @@
+import org.apache.spark._
+import org.apache.spark.sql._
+import org.apache.spark.sql.types.{IntegerType, StringType}
+
+object AltimetricTestBootstrap extends App{
+
+  val spark = SparkSession.builder().master("local[2]").appName("app-PalindromeTester").getOrCreate()
+  val rawRdd = spark.sparkContext.parallelize(Seq(("banana"),("oppoisoppo")))
+
+  // rawRdd.foreach(println(_))
+spark.sparkContext.
+  rawRdd.foreach(palindromeTest(_))
+
+  def palindromeTest(s:String) = {
+
+    var list = scala.collection.mutable.MutableList[String]()
+    var listOfPalindromes = scala.collection.mutable.MutableList[String]()
+
+    val givenString = s
+
+    var temp = 0
+    val n = givenString.length - 1
+
+    for (j <- 0 to n) {
+      for (k <- j to n) {
+        var definedSubstring = givenString.substring(j,k+1)
+        list = list.:+(definedSubstring)
+      }
+    }
+
+    // list.foreach(println(_))
+
+    list.foreach(myString => {
+
+      var reversedString = ""
+
+      if(myString.length > 1)
+      {
+
+        for (i <- (0 to myString.length - 1).reverse) {
+          reversedString = reversedString + myString(i)
+        }
+
+        // println("The reversed string is " + reversedString)
+
+        if (myString == reversedString) {
+          println(s"The string ${reversedString} is a palindrome")
+          listOfPalindromes = listOfPalindromes.:+(myString)
+        }
+
+      }
+
+    })
+
+    println("LISTOFPALINDROME:::::::")
+    listOfPalindromes.foreach(println(_))
+
+    val listOfTuples = listOfPalindromes.map(x => (x,x.length))
+
+    val sortedList = listOfTuples.sortBy(x => x._2)
+    sortedList.foreach(println(_))
+
+    println("The largest palindrome in a given string is " +sortedList(sortedList.length-1))
+
+  }
+}
--- SparkLearning/src/main/scala/Bootstrap.scala
+++ SparkLearning/src/main/scala/Bootstrap.scala
@@ -1,6 +1,5 @@
 
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.types
+import org.apache.spark.sql.{RelationalGroupedDataset, SparkSession, types}
 import org.apache.spark._
 import org.apache.spark.sql.types.{IntegerType, StringType}
 
@@ -47,14 +46,16 @@ object Bootstrap extends App {
 
   import org.apache.spark.sql.functions._
 
-// https://docs.databricks.com/spark/latest/dataframes-datasets/complex-nested-data.html 
+// https://docs.databricks.com/spark/latest/dataframes-datasets/complex-nested-data.html
+
+  //get_json_object():This method extracts a JSON object from a JSON string based on JSON path specified
   val rawDf =  Seq (
     (0, """{"device_id": 0, "device_type": "sensor-ipad", "ip": "68.161.225.1", "cca3": "USA", "cn": "United States", "temp": 25, "signal": 23, "battery_level": 8, "c02_level": 917, "timestamp" :1475600496 }"""),
     (1, """{"device_id": 1, "device_type": "sensor-igauge", "ip": "213.161.254.1", "cca3": "NOR", "cn": "Norway", "temp": 30, "signal": 18, "battery_level": 6, "c02_level": 1413, "timestamp" :1475600498 }"""),
     (2, """{"device_id": 2, "device_type": "sensor-ipad", "ip": "88.36.5.1", "cca3": "ITA", "cn": "Italy", "temp": 18, "signal": 25, "battery_level": 5, "c02_level": 1372, "timestamp" :1475600500 }""")).toDF("id", "device")
 
-  /*rawDf.printSchema()
-  rawDf.show()*/
+  rawDf.printSchema()
+  rawDf.show(false)
 
 
   val modifiedDf = rawDf.withColumn("test",get_json_object(col("device"),"$.device_type")).drop(col("device"))
@@ -81,8 +82,20 @@ object Bootstrap extends App {
       dayofmonth(col("messageTimestamp")).as("dayofmonth")
     )
 
-  dtDf.show()
+ // dtDf.show()
 
 
+  val mtDf = dtDf.withColumn("id",monotonicallyIncreasingId)
+ // mtDf.show()
+
+  // pivot method don't return dataframe. agg operation should be there after pivot
+val df5 = spark.sparkContext.parallelize(Seq(("Vishal","Hindi",60),("Vishal","Hindi",60),("Vishal","Maths",60),("Vishal","Science",60)))
+  .toDF("name","subject","marks")
+
+  df5.show()
+
+  val df6 = df5.groupBy(col("name")).pivot("subject").sum("marks")
+
+  df6.show()
 
 }
